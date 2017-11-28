8953366fadd5966748c62c410e5ad30ef9f8a04d
Jarret Moses <jarret.moses@gmail.com>
2017-11-27 22:01:45 -0500

    chore: add data constant

--- src/screens/example-one-screen/example-one-screen.js
+++ src/screens/example-one-screen/example-one-screen.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
 
 import Card from '../../components/card/card';
+import data from '../../constants/data';
 import styles from './styles';
-
-const arr = [1,2,3,4,5,6,7,8,9]
 
 export default class MainScreen extends Component {
   _renderCards = () => (
-    arr.map((number) => <Card number={number} key={number} />)
+    data.map((number) => <Card number={number} key={number} />)
   )
 
   render() {
